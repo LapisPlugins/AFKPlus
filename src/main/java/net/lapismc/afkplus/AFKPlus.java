4370127c14594eee606f408a771bde9b1494c55a
dart2112 <benjaminmartin@live.com>
2017-01-20 21:02:44 +1030

    Moved updating to update() added copyright

--- src/main/java/net/lapismc/afkplus/AFKPlus.java
+++ src/main/java/net/lapismc/afkplus/AFKPlus.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 Benjamin Martin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.lapismc.afkplus;
 
 import net.lapismc.afkplus.commands.AFKPlusAFK;
@@ -27,6 +43,15 @@ public final class AFKPlus extends JavaPlugin {
         this.getCommand("afkplus").setExecutor(new net.lapismc.afkplus.commands.AFKPlus(this));
         this.getCommand("afk").setExecutor(new AFKPlusAFK(this));
         saveDefaultConfig();
+        Metrics metrics = new Metrics(this);
+        metrics.start();
+        AFKListeners = new AFKPlusListeners(this);
+        AFKConfig = new AFKPlusConfiguration(this);
+        Bukkit.getPluginManager().registerEvents(AFKListeners, this);
+        startTimer();
+    }
+
+    private void update() {
         updater = new LapisUpdater(this, "AFKPlus", "Dart2112", "AFKPlus", "master");
         if (updater.checkUpdate("AFKPlus")) {
             if (getConfig().getBoolean("UpdateDownload")) {
@@ -37,12 +62,6 @@ public final class AFKPlus extends JavaPlugin {
         } else {
             logger.info("No update available for AFKPlus");
         }
-        Metrics metrics = new Metrics(this);
-        metrics.start();
-        AFKListeners = new AFKPlusListeners(this);
-        AFKConfig = new AFKPlusConfiguration(this);
-        Bukkit.getPluginManager().registerEvents(AFKListeners, this);
-        startTimer();
     }
 
     private Runnable runnable(AFKPlus plugin) {
